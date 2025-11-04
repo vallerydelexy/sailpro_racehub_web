497bb5da2eb4d057dfb89f04ff5c352b42baf8fe
Rizki Aprita <rizki.aprita@clearscope.solutions>
2025-11-04 13:25:31 +0700

    build prod

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d2913632a4578ee4d0b8b1c4a69888c8a0672c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "981675160"
+    serviceWorkerVersion: "1838186149"
   }
 });
